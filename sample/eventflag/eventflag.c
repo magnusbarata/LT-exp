f05cbb0b80a48f42947ebd8c32cb0cf9fa3df275
impreplus <plusaachronos800@gmail.com>
2018-11-29 14:02:18 +0900

    Add .gitignore

--- sample/eventflag/eventflag.c
+++ sample/eventflag/eventflag.c
@@ -7,24 +7,26 @@
 
 #include "defs.h"
 
-typedef enum {
-	eRtouch = 1 << 0,	// Touch Sensor (Right)
-	eLtouch = 1 << 1,	// Touch Sensor (Left)
+typedef enum
+{
+	eRtouch = 1 << 0, // Touch Sensor (Right)
+	eLtouch = 1 << 1, // Touch Sensor (Left)
 } EBits;
 
 #define Rtouch NXT_PORT_S1
 #define Ltouch NXT_PORT_S4
 
 // main program
-void
-MainTsk(VP_INT exinf)
+void MainTsk(VP_INT exinf)
 {
-	FLGPTN	sensor;
+	FLGPTN sensor;
 
 	dly_tsk(1000);
-	for (;;) {
+	for (;;)
+	{
 		wai_flg(Fsens, eRtouch | eLtouch, TWF_ORW, &sensor);
-		switch (sensor) {
+		switch (sensor)
+		{
 		case eRtouch:
 			ecrobot_sound_tone(440, 100, 60);
 			break;
@@ -44,40 +46,40 @@ MainTsk(VP_INT exinf)
 }
 
 // Event flag update routine
-void
-ESetTsk(VP_INT exinf)
+void ESetTsk(VP_INT exinf)
 {
-	for (;;) {
+	for (;;)
+	{
 		dly_tsk(10);
-		if (ecrobot_get_touch_sensor(Rtouch)) {
+		if (ecrobot_get_touch_sensor(Rtouch))
+		{
 			set_flg(Fsens, eRtouch);
 		}
-		if (ecrobot_get_touch_sensor(Ltouch)) {
+		if (ecrobot_get_touch_sensor(Ltouch))
+		{
 			set_flg(Fsens, eLtouch);
 		}
 	}
 }
 
 // NXT-JSP hook ISR to be invoked every 1msec
-void
-jsp_systick_low_priority(void)
+void jsp_systick_low_priority(void)
 {
-	if (get_OS_flag()) {
+	if (get_OS_flag())
+	{
 		isig_tim();
 		check_NXT_buttons();
 	}
 }
 
 // System Initialize Routine
-void
-ecrobot_device_initialize(void)
+void ecrobot_device_initialize(void)
 {
 	;
 }
 
 // System Shutdown Routine
-void
-ecrobot_device_terminate(void)
+void ecrobot_device_terminate(void)
 {
 	;
 }
