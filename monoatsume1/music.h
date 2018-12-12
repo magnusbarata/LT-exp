9f2da307a4c82c9e614dcb8bc369b0d968aab2dd
magnusbarata <barata.magnuz@gmail.com>
2018-12-13 01:05:31 +0900

    cleanup

---
+++ monoatsume1/music.h
@@ -0,0 +1,14 @@
+/*
+ * music.h
+ *	音楽演奏用ヘッダファイル
+ */
+
+#ifndef MUSIC_H
+#define MUSIC_H
+
+int play_notes(int, int, char *);
+
+extern char *chiba_univ;
+#define TIMING_chiba_univ	(2222)
+
+#endif
